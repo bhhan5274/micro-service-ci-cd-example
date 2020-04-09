38657f2193ba8adc360a5adc7864037d576bb161
bhhan <hbh5274@gmail.com>
2020-04-09 21:32:36 +0900

    micro-service CI/CD Example first commit

---
+++ licensing-service/src/main/docker/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "====================================================="
+echo "Starting License Server"
+echo "====================================================="
+
+java -jar /usr/local/licensingservice/@project.build.finalName@.jar
